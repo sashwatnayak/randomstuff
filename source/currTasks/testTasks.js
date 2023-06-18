6049aa50f50799a70ecbf4adbe8ad1d2afeb15e2
snayak <sashwatnayak3@gmail.com>
2023-06-18 09:59:43 -0700

    modified!

--- source/currTasks/testTasks.js
+++ source/currTasks/testTasks.js
@@ -1,9 +1,15 @@
 import { Task } from './../skyTasks.js';
 
-const first = new Task('first', Task.getUniqueUID(), 2, new Date());
-first.data.ddl = new Date('November 23, 2022 23:59:00');  // Deadline: November 23nd, 11:59pm 
+const first = new Task('second', Task.getUniqueUID(), 2, new Date());
+first.data.ddl = new Date('November 23, 2023 23:59:00');  // Deadline: November 23nd, 11:59pm 
 first.data.duration = 1;
 first.data.priority = 5;
 first.addToLocalStorage();
+
+const second = new Task('damn', Task.getUniqueUID(), 7, new Date('June 4, 2023 23:59:00'));
+second.data.ddl = new Date('June 7, 2023 23:59:00');  // Deadline: November 23nd, 11:59pm 
+second.data.duration = 1;
+second.data.priority = 5;
+second.addToLocalStorage();
 
 Task.schedule();
